ad383527fbc1b5f71fc2fc03fbda4e12c9e2f418
mahmoud140 <34834864+mahmoud140@users.noreply.github.com>
2018-02-23 20:21:24 +0200

    intial the db and generate dumy cookies for check if the cookies enabled

---
+++ souq_java/src/java/com/java/CheckCookiesEnabled.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.java;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Mahmoud
+ */
+public class CheckCookiesEnabled extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Cookie[] cookies = request.getCookies();
+        if (!(cookies == null)) {
+            Cookie cookie = cookies[0];
+//            if ((cookie.getName().equalsIgnoreCase("check")) && cookie.getValue().equalsIgnoreCase("true")) {
+                response.getWriter().println("cookie is enable");
+                String uname = request.getParameter("name");
+                String pass = request.getParameter("password");
+                response.sendRedirect("login?name=" + uname + "&password=" + pass);
+//            }
+        } else {
+            response.getWriter().println("cookie is disabled");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
---
+++ souq_java/src/java/com/java/InitListener.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.java;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Web application lifecycle listener.
+ *
+ * @author Mahmoud
+ */
+public class InitListener implements ServletContextListener, HttpSessionListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        try {
+            DriverManager.registerDriver(new org.postgresql.Driver());
+            Connection conn= DriverManager.getConnection("jdbc:postgresql://127.0.0.1/souq", "postgres", "iti");
+            sce.getServletContext().setAttribute("conn", conn);
+        } catch (SQLException ex) {
+            Logger.getLogger(InitListener.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        
+    }
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+    }
+}
