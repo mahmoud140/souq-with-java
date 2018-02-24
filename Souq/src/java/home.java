994e469c52bdf882722cc5e1f4cee6fd07a91029
mahmoud140 <34834864+mahmoud140@users.noreply.github.com>
2018-02-24 09:57:16 +0200

    optimize home.java

--- Souq/src/java/home.java
+++ Souq/src/java/home.java
@@ -28,9 +28,7 @@ public class home extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-      request.getRequestDispatcher("SouqHeader.html").include(request, response); 
-      
-      
+        request.getRequestDispatcher("SouqHeader.html").include(request, response);
         request.getRequestDispatcher("SouqFooter.html").include(request, response);
     }
 
@@ -48,29 +46,4 @@ public class home extends HttpServlet {
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
