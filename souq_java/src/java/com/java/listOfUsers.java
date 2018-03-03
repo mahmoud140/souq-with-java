e76fc6ecb5427f8905f7ae36315db362a62ffca5
mohamedkhapiry <e.mohamedkhapiry@yahoo.com>
2018-03-03 19:28:48 +0200

    updates

--- souq_java/src/java/com/java/listOfUsers.java
+++ souq_java/src/java/com/java/listOfUsers.java
@@ -39,7 +39,7 @@ public class listOfUsers extends HttpServlet {
         String uName=request.getParameter("UserName");
         int userBudget=(Integer.valueOf(request.getParameter("user_budget")));
         insert.edit(uName,userBudget);
-        response.sendRedirect("");
+        response.sendRedirect("/souq_java/listOfUsers.jsp");
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
